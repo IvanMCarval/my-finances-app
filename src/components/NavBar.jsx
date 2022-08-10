6bb906f617c5fced770a294a0ff91a3397628e6b
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-08-10 10:19:09 -0300

    correção de style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid" style={{margin: '0 300px 0 300px'}}>
+      <div className="container-fluid">
         <Link className="navbar-brand" to={"/"}>Minhas Finanças</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,22 @@ import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <div className="card-2">
-      <div className="jumbotron">
-        <h1 className="display-3">Bem vindo!</h1>
-        <p className="lead">Esse é seu sistema de finanças.</p>
-        <p className="lead">Seu saldo para o mês atual é de R$ 5.325,21</p>
-        <hr className="my-4" />
-        <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
-        <p className="lead">
-          <Link className="btn btn-info btn-lg" to={"/cadastrar-usuario"} style={{ marginRight: '15px' }}>Cadastrar Usuário</Link>
-          <Link className="btn btn-danger btn-lg" to={"/"}>Cadastrar Lançamento</Link>
-        </p>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop:'100px'}}>
+      <div className="card-2">
+        <div className="jumbotron">
+          <h1 className="display-3">Bem vindo!</h1>
+          <p className="lead">Esse é seu sistema de finanças.</p>
+          <p className="lead">Seu saldo para o mês atual é de R$ 5.325,21</p>
+          <hr className="my-4" />
+          <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
+          <p className="lead">
+            <Link className="btn btn-info btn-lg" to={"/cadastrar-usuario"} style={{ marginRight: '15px' }}>Cadastrar Usuário</Link>
+            <Link className="btn btn-danger btn-lg" to={"/"}>Cadastrar Lançamento</Link>
+          </p>
+        </div>
       </div>
     </div>
+
 
   )
 }
