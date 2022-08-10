f3fc031c35665e24584e7c950bbe164bf5c9a3a6
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-08-10 12:14:03 -0300

    implementado axios e requisições de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,55 @@
+import axios from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { Card } from "../components/Card";
 
 export function Login() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+  const navigate = useNavigate()
 
-  function logar(e) {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const [mensagemErro, setMensagemErro] = useState('')
+
+  function validarCamposLogin(e) {
     e.preventDefault();
 
     if (email != '' && senha != '') {
       setEmail(email);
       setSenha(senha);
+
+      logar()
     } else {
       alert("informe email e senha")
     }
   }
 
+  function logar() {
+    axios.post('http://localhost:8080/api/usuarios/autenticar', {
+      email: email,
+      senha: senha
+    }).then(function (response) {
+      navigate('/home')
+    }).catch(function (erro) {
+      setMensagemErro(erro.response.data)
+    })
+  }
+
 
   return (
-    <div className="container" style={{padding: '100px'}}>
-      <div className="column" style={{display: 'flex', justifyContent: 'center'}}>
+    <div className="container" style={{ padding: '100px' }}>
+      <div className="column" style={{ display: 'flex', justifyContent: 'center' }}>
         <div className="col-md-13">
           <div className="bs-docs-section">
             <Card title="Login">
+              <div>
+                <span style={{color: 'orange'}}>{mensagemErro}</span>
+              </div>
               <div className="row">
                 <div className="col-lg-12">
                   <div className="bs-component">
-                    <form onSubmit={logar}>
+                    <form onSubmit={validarCamposLogin}>
                       <fieldset>
                         <div className="form-group" style={{ marginTop: '-50px' }}>
                           <div className="form-group">
